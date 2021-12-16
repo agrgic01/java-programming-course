9a734a095def809ff9156eeac3902e5839ce7a0a
Alen <alen.grgic95@gmail.com>
2021-12-16 05:20:26 +0100

    Added CE27InputCalculator - December 16, 2021

---
+++ src/com/agrgic/CodingExercises/CE27InputCalculator/Main.java
@@ -0,0 +1,15 @@
+package com.agrgic.CodingExercises.CE27InputCalculator;
+
+import java.util.Scanner;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        InputCalculator.inputThenPrintSumAndAverage();
+
+    } // main method
+
+
+} // Main class

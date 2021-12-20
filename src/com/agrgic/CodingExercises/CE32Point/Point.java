5eee304c1ef951b9dd377771b0d8d050f2a21bce
Alen <alen.grgic95@gmail.com>
2021-12-20 14:33:09 +0100

    Dec20; CE32: Point

---
+++ src/com/agrgic/CodingExercises/CE32Point/Point.java
@@ -0,0 +1,55 @@
+package com.agrgic.CodingExercises.CE32Point;
+
+public class Point {
+
+
+    private int x;
+    private int y;
+
+
+    /* CONSTRUCTORS */
+    public Point(int x, int y) {
+
+        this.x = x;
+        this.y = y;
+
+    }
+
+    public Point() {}
+
+
+    /* GETTERS */
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+
+    /* SETTERS */
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+
+    /* METHOD */
+    public double distance() {
+        return Math.sqrt((-this.x) * (-this.x) + (-this.y) * (-this.y));
+    }
+
+    public double distance(int x, int y) {
+        return Math.sqrt((x - this.x) * (x - this.x) + (y - this.y) * (y - this.y));
+    }
+
+    public double distance(Point another) {
+        return Math.sqrt((another.x - this.x) * (another.x - this.x) + (another.y - this.y) * (another.y - this.y));
+    }
+
+
+} // Point class

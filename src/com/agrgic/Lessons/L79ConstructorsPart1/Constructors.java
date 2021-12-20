06b8ec139bdaea746d8ea25ff43e4586b82bb093
Alen <alen.grgic95@gmail.com>
2021-12-20 08:36:46 +0100

    December 19, 2021 - lecture 80 + challenge

--- src/com/agrgic/Lessons/L79ConstructorsPart1/Constructors.java
+++ src/com/agrgic/Lessons/L79ConstructorsPart1/Constructors.java
@@ -5,6 +5,7 @@ public class Constructors {
 
     public static void main(String[] args) {
 
+        /*
         BankAccount bobsAccount = new BankAccount(); //(123456, 0.00, "Josh Smith", "josh@email.com", "092323146");
 
         System.out.println(bobsAccount.getAccountNumber());
@@ -25,6 +26,26 @@ public class Constructors {
         BankAccount timsAccount = new BankAccount("Tim", "tim@email.com", "094123123");
 
         System.out.println("account number: " + timsAccount.getAccountNumber() + " name: " + timsAccount.getCustomerName());
+        */
+
+        VipCustomer person1 = new VipCustomer();
+        System.out.println(person1.getName());
+        System.out.println(person1.getCreditLimit());
+        System.out.println(person1.getEmail());
+
+        System.out.println("\r");
+
+        VipCustomer person2 = new VipCustomer("Bob", 50_000.00);
+        System.out.println(person2.getName());
+        System.out.println(person2.getCreditLimit());
+        System.out.println(person2.getEmail());
+
+        System.out.println("\r");
+
+        VipCustomer person3 = new VipCustomer("Alen", 420_000.00, "alen@alen.com");
+        System.out.println(person3.getName());
+        System.out.println(person3.getCreditLimit());
+        System.out.println(person3.getEmail());
 
     } // main method
 

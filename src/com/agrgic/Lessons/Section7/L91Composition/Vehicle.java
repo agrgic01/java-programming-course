22350020afa9326537d02c3082dd96981521ca13
Alen <alen.grgic95@gmail.com>
2021-12-28 01:50:08 +0100

    Dec27; L91

---
+++ src/com/agrgic/Lessons/Section7/L91Composition/Vehicle.java
@@ -0,0 +1,16 @@
+package com.agrgic.Lessons.Section7.L91Composition;
+
+public class Vehicle {
+
+    /* field */
+    private String name;
+
+    /* constructor */
+    public Vehicle(String name) {
+        this.name = name;
+    }
+
+    /*  */
+
+
+} // Vehicle class

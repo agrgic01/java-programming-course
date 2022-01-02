d4d4f01a884a91ea9f0106c81b8282722d932da0
Alen <alen.grgic95@gmail.com>
2022-01-02 23:50:29 +0100

    Jan02; CE39

---
+++ src/com/agrgic/CodingExercises/CE39Polymorphism/Main.java
@@ -0,0 +1,19 @@
+package com.agrgic.CodingExercises.CE39Polymorphism;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Car car = new Car(8, "Base car");
+        System.out.println(car.startEngine());
+        System.out.println(car.accelerate());
+        System.out.println(car.brake());
+
+        Mitsubishi mitsubishi = new Mitsubishi(6, "Outlander vrx 4wd");
+        System.out.println(mitsubishi.startEngine());
+        System.out.println(mitsubishi.accelerate());
+        System.out.println(mitsubishi.brake());
+
+    } // main method
+
+} // Main class

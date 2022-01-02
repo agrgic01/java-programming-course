0d13dc77c405d5eb00e80756cbde638d64772e88
Alen <alen.grgic95@gmail.com>
2022-01-03 00:06:25 +0100

    Jan02; CE39

--- src/com/agrgic/CodingExercises/CE39Polymorphism/Main.java
+++ src/com/agrgic/CodingExercises/CE39Polymorphism/Main.java
@@ -14,6 +14,16 @@ public class Main {
         System.out.println(mitsubishi.accelerate());
         System.out.println(mitsubishi.brake());
 
+        Holden holden = new Holden(6, "Holden Commodore");
+        System.out.println(holden.startEngine());
+        System.out.println(holden.accelerate());
+        System.out.println(holden.brake());
+
+        Ford ford = new Ford(6, "Ford Falcon");
+        System.out.println(ford.startEngine());
+        System.out.println(ford.accelerate());
+        System.out.println(ford.brake());
+
     } // main method
 
 } // Main class

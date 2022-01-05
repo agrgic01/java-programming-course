ca2e14402b07fdece7be4be61956e867ac7a5713
Alen <alen.grgic95@gmail.com>
2022-01-06 00:49:59 +0100

    Jan04; CE40

--- src/com/agrgic/CodingExercises/CE40BillsBurgers/Main.java
+++ src/com/agrgic/CodingExercises/CE40BillsBurgers/Main.java
@@ -4,6 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Hamburger hamburger = new Hamburger("Basic", "Sausage", 3.56, "White");
+        double price = hamburger.itemizeHamburger();
+        hamburger.addHamburgerAddition1("Tomato", 0.27);
+        hamburger.addHamburgerAddition2("Lettuce", 0.3);
+        hamburger.addHamburgerAddition3("Cheese", 1.12);
+        System.out.println("Total burger price is " + hamburger.itemizeHamburger());
     } // main method
 
 } // Main class

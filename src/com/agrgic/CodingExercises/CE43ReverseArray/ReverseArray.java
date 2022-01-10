6eb9d303d5463831d5562fef9ba24a1637b62e5e
Alen <alen.grgic95@gmail.com>
2022-01-10 07:29:44 +0100

    Jan09; CE43

---
+++ src/com/agrgic/CodingExercises/CE43ReverseArray/ReverseArray.java
@@ -0,0 +1,34 @@
+package com.agrgic.CodingExercises.CE43ReverseArray;
+
+import java.util.Arrays;
+
+public class ReverseArray {
+
+
+    public static void main(String[] args) {
+
+        int[] array = {1, 2, 3, 4, 5};
+
+        reverse(array);
+
+    } //main method
+
+
+    private static void reverse(int[] array) {
+
+        System.out.println("Array = " + Arrays.toString(array));
+
+        int maxValue = array.length - 1;
+        int halfArray = array.length / 2;
+
+        for (int i = 0; i < halfArray; i++) {
+            int temp = array[i];
+            array[i] = array[maxValue - i];
+            array[maxValue - i] = temp;
+        }
+
+        System.out.println("Reversed array = " + Arrays.toString(array));
+
+    } //reverse method
+
+} //ReverseArray class
